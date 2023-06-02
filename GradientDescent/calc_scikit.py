11f7a94388326eaa235007097e74e20bd88f3db7
imaitygit <i.maity@imperial.ac.uk>
2023-06-02 10:11:53 +0530

    Adding Neuralnetwork

--- GradientDescent/calc_scikit.py
+++
@@ -1,8 +0,0 @@
-
-import numpy as np
-import matplotlib.pyplot as plt
-
-data = np.genfromtxt("./data/house.txt", unpack=True, delimiter=",",\
-                     skip_header=1).T
-gdwithscikit = GDwithscikit(data, verbose=False)
-gdwithscikit.compare()
